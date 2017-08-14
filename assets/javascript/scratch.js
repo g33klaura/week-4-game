e5e7976b97f87d866b8fabbe6de6f42bb660be3e
Laura Boetcher <g33klaura@gmail.com>
2017-08-14 16:07:26 -0400

    Add 'scratch paper' file to house code not yet working and things to try

---
+++ assets/javascript/scratch.js
@@ -0,0 +1,40 @@
+/* 
+* JS SCRATCH PAPER
+*
+* Needed a place to keep snippets I've tried, or have yet to try
+*
+*/
+
+/* Want fancy fade in/out on hover if can get imageClick function to work!!
+function imageClick() {    
+    $('#1st').mouseenter(function() {
+        $('#1st').fadeTo('fast', 0.5);
+    });
+
+    $('#1st').mouseleave(function() {
+        $('#1st').fadeTo('fast', 1);
+    });
+}
+*/
+
+
+// IMAGES NEED NUMBERS ASSIGNED TO THEM
+/*
+for (var heroValueIndex = 0; heroValueIndex < values.length; heroValueIndex++) {
+*/
+
+/*
+    // Think this needs to be in a for loop function
+    var hero = $('<img>');
+
+    // Adds a class of hero-image to the images in <ul>?
+    hero.addClass('hero-image');
+    // Image already on page
+    // Hopefully sets value from the options from values
+    hero.attr('data-herovalue', values[heroValueIndex]);
+
+    $('.crystal-heroes').append(hero);
+
+    // FOR TESTING
+    console.log(hero[heroValueIndex]);
+*/
