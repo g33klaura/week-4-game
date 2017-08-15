364f3f38fc790b8c8fe65242e8dac0e4bc7ee982
Laura Boetcher <g33klaura@gmail.com>
2017-08-15 00:26:41 -0400

    Don't want to miss any edits

--- assets/javascript/scratch.js
+++ assets/javascript/scratch.js
@@ -5,6 +5,22 @@
 *
 */
 
+/* 
+Did this if/else statement before turning it into a function to get the random non-repeating numbers values for the images
+
+            if (holderArray.indexOf(value) !== -1) {
+
+            value = Math.floor(Math.random() * (12 - 1)) + 1;
+            $('#' + crystalHeroes[crystalHeroIndex]).attr('data-imagevalue', value);
+
+            //append(values)
+            } else {
+                $('#' + crystalHeroes[crystalHeroIndex]).attr('data-imagevalue', value);
+            }
+        */
+
+
+
 /* Want fancy fade in/out on hover if can get imageClick function to work!!
 function imageClick() {    
     $('#1st').mouseenter(function() {
@@ -18,10 +34,27 @@ function imageClick() {
 */
 
 
+
+// FROM EXAMPLE: Each imageCrystal will be given a data attribute called data-crystalValue.
+        // This data attribute will be set equal to the array value.
+
+        /*
+            imgValue.attr('data-imagevalue', crystalHeroes[crystalHeroIndex]);
+            console.log(addValueToImage);
+
+        */
+
+        // THIS WAS FROM EXAMPLE GAME - BUT MY IMAGES ARE STATIC ON PAGE..... NEEDS RENAMING. SEE ABOVE.
+        // $('.hero-image').on('click', function() {
+
+
+
 // IMAGES NEED NUMBERS ASSIGNED TO THEM
 /*
 for (var heroValueIndex = 0; heroValueIndex < values.length; heroValueIndex++) {
 */
+
+
 
 /*
     // Think this needs to be in a for loop function
